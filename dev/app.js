ed197944e72d7641d3d85ede8b0564d96d1dfd2c
Alexander C Perry <acp96@cornell.edu>
2020-06-15 12:38:56 -0400

    prevented multiple error msgs

--- dev/app.js
+++ dev/app.js
@@ -2,15 +2,20 @@ const nameInput = document.getElementById('name-input');
 const nameFormBtn = document.getElementsByClassName('submit-btn')[0];
 const introForm = document.getElementsByClassName('intro-form')[0];
 const introDiv = document.getElementsByClassName('intro')[0];
+let hasErrors = false;
 
 nameFormBtn.addEventListener('click', e => {
   if (nameInput.value.trim() === "") {
     e.preventDefault();
 
-    const errorDiv = document.createElement('div');
-    errorDiv.className = 'error-div';
-    errorDiv.innerHTML = `<p>Please submit a non-empty name</p>`;
-    introForm.insertBefore(errorDiv, nameInput);
-    introDiv.style.height = "20%";
+    if (!hasErrors) {
+
+      const errorDiv = document.createElement('div');
+      errorDiv.className = 'error-div';
+      errorDiv.innerHTML = `<p>Please submit a non-empty name</p>`;
+      introForm.insertBefore(errorDiv, nameInput);
+      introDiv.style.height = "20%";
+      hasErrors = true;
+    }
   }
 })
